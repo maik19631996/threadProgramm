cf4694937fcd6383ab83bd96232c14d236fd29d3
maik <maikbergner@freenet.de>
2018-05-10 11:54:18 +0200

    adding a useful test to FieleTreeMarsschaller.marschallto

--- src/test/java/JaxB/FieleTreeMarshallerTest.java
+++ src/test/java/JaxB/FieleTreeMarshallerTest.java
@@ -22,28 +22,29 @@ import static org.junit.Assert.*;
  * @author maik
  */
 public class FieleTreeMarshallerTest {
-    
+
     public FieleTreeMarshallerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
-     * Test of marshallTo method, of class FieleTreeMarshaller.
+     * Test of marshallTo method, of class FieleTreeMarshaller.t test a simple
+     * case of xml Generation with Verzeichnis Schema
      */
     @Test
     public void testMarshallTo() throws Exception {
@@ -51,27 +52,34 @@ public class FieleTreeMarshallerTest {
         Verzeichnis rootVerzeichnis = new Verzeichnis();
         rootVerzeichnis.grosse = "50MB";
         rootVerzeichnis.name = "myFirsttestFile";
-        ArrayList<Datei> datein = new ArrayList<Datei>();
+        ArrayList<Datei> datein = new ArrayList<>();
         Datei datei = new Datei();
         datei.grosse = "50k";
         datei.name = "cool File";
         datein.add(datei);
-        ArrayList<Verzeichnis> verzeichnisse = new ArrayList<Verzeichnis>();
+        ArrayList<Verzeichnis> verzeichnisse = new ArrayList<>();
         Verzeichnis subdir = new Verzeichnis();
         subdir.grosse = "100k";
         subdir.name = "subdirName";
         verzeichnisse.add(subdir);
         rootVerzeichnis.subDirectoryList = verzeichnisse;
-                
+
         rootVerzeichnis.subFileList = datein;
         FieleTreeMarshaller instance = new FieleTreeMarshaller();
+
         OutputStream expResult = null;
         ByteArrayOutputStream result = (ByteArrayOutputStream) instance.marshallTo(rootVerzeichnis);
         String xmlString = new String(result.toByteArray());
-        System.out.print(xmlString);
-        //assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        //System.out.print(xmlString); not needed in normal case
+
+        String assertingString
+                = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                + "<Verzeichnis name=\"myFirsttestFile\" grosse=\"50MB\">\n "
+                + "   <verzeichis name=\"subdirName\" grosse=\"100k\"/>\n "
+                + "   <datei name=\"cool File\" grosse=\"50k\"/>\n"
+                + "</Verzeichnis>\n";
+        assertEquals(xmlString, assertingString);
 
     }
-    
+
 }
